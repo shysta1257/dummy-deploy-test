00b261e0ca0e0c9809523799807201baa15c3853
Shysta <shysta1257@gmail.com>
2026-09-14 16:34:29 +0530

    changed heading text

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ def home():
     <html>
       <head><title>Dummy Deploy Test</title></head>
       <body style="font-family: sans-serif; text-align: center; margin-top: 100px;">
-        <h1>🎉 It's live!</h1>
+        <h1>It's live!</h1>
         <p>If you're seeing this on a real URL (not localhost), your deployment worked.</p>
+        <p>I just found out how deployment actually works.</p>
       </body>
     </html>
     """
